778881843c3c0b7ca1dcb97033b42c90417f43c8
Abdulmoez Derrija <abdulmoezderrija@gmail.com>
2022-09-04 17:02:28 +0300

    initial commit

---
+++ src/main/java/com/example/aluminumcompositepaneldemoapplication/AluminumCompositePanelDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.aluminumcompositepaneldemoapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluminumCompositePanelDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AluminumCompositePanelDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aluminumcompositepaneldemoapplication/AluminumCompositePanelDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aluminumcompositepaneldemoapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluminumCompositePanelDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
