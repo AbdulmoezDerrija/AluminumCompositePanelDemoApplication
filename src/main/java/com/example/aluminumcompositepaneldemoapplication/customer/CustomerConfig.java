b4d12719ea570d476922ddd5701a800e9628f4d8
Abdulmoez Derrija <abdulmoezderrija@gmail.com>
2022-09-08 22:29:31 +0300

    CustomerConfig created. using CommandLineRunner and CustomerRepository as variable to save the customers to the database.

---
+++ src/main/java/com/example/aluminumcompositepaneldemoapplication/customer/CustomerConfig.java
@@ -0,0 +1,44 @@
+package com.example.aluminumcompositepaneldemoapplication.customer;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class CustomerConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(CustomerRepository repository) {
+        return args -> {
+            Customer ahmed = new Customer(
+                    1L,
+                    "Ahmed",
+                    "Steve",
+                    "AhmedSteve@gmail.com",
+                    "Libyan",
+                    20,
+                    LocalDate.of(2002, Month.JANUARY, 25),
+                    "0925752564",
+                    "Tripoli, Algraba street"
+            );
+            Customer alex = new Customer(
+                    "Alex",
+                    "Ali",
+                    "AlexAli@gmail.com",
+                    "Libyan",
+                    21,
+                    LocalDate.of(2001, Month.FEBRUARY, 9),
+                    "0924837483",
+                    "Tripoli, Aldol street"
+            );
+
+            repository.saveAll(
+                    List.of(ahmed, alex)
+            );
+        };
+    }
+}
--- src/main/java/com/example/aluminumcompositepaneldemoapplication/customer/CustomerService.java
+++ src/main/java/com/example/aluminumcompositepaneldemoapplication/customer/CustomerService.java
@@ -1,28 +1,23 @@
 package com.example.aluminumcompositepaneldemoapplication.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class CustomerService {
 
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     @GetMapping
     public List<Customer> getCustomers() {
-        return List.of(
-                new Customer(
-                        1L,
-                        "Ahmed",
-                        "Steve",
-                        "AhmedSteve@gmail.com",
-                        "Libyan",
-                        20,
-                        LocalDate.of(2002, Month.JANUARY, 25),
-                        "0925752564",
-                        "Tripoli, Algraba street"
-                ));
+        return customerRepository.findAll();
     }
 }
