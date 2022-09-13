29d658adf6fec4fc04f95c37b06f07324d8b8128
Abdulmoez Derrija <abdulmoezderrija@gmail.com>
2022-09-13 09:46:46 +0300

    create AluminumCompositePanelconfig.

---
+++ src/main/java/com/example/aluminumcompositepaneldemoapplication/aluminumcompositepanel/AluminumCompositePanelConfig.java
@@ -0,0 +1,7 @@
+package com.example.aluminumcompositepaneldemoapplication.aluminumcompositepanel;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AluminumCompositePanelConfig {
+}
--- src/main/java/com/example/aluminumcompositepaneldemoapplication/aluminumcompositepanel/AluminumCompositePanelRepository.java
+++ src/main/java/com/example/aluminumcompositepaneldemoapplication/aluminumcompositepanel/AluminumCompositePanelRepository.java
@@ -1,6 +1,8 @@
 package com.example.aluminumcompositepaneldemoapplication.aluminumcompositepanel;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AluminumCompositePanelRepository extends JpaRepository<AluminumCompositePanel, String> {
 }
