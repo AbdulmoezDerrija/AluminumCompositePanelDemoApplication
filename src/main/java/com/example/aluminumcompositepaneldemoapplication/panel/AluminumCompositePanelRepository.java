fb53df5e8237e10ec8c0feca0e113e878d51bc82
Abdulmoez Derrija <abdulmoezderrija@gmail.com>
2022-09-13 09:30:20 +0300

    create AluminumCompositePanelRepository.

---
+++ src/main/java/com/example/aluminumcompositepaneldemoapplication/panel/AluminumCompositePanelRepository.java
@@ -0,0 +1,6 @@
+package com.example.aluminumcompositepaneldemoapplication.panel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AluminumCompositePanelRepository extends JpaRepository<AluminumCompositePanel, String> {
+}
