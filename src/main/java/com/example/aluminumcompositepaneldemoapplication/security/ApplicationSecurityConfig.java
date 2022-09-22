aa2cb16587f92aca3eef09095ab8e15f82d90897
Abdulmoez Derrija <abdulmoezderrija@gmail.com>
2022-09-22 11:03:38 +0300

    create index.html. adding antMatchers to only use the basic auth (configure) of specific Urls

--- src/main/java/com/example/aluminumcompositepaneldemoapplication/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/aluminumcompositepaneldemoapplication/security/ApplicationSecurityConfig.java
@@ -13,6 +13,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/", "/css/*", "/js/*", "index")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
